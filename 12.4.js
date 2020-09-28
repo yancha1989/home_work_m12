afa34b1f0cfccc756fc6764c8288a435da226a29
yancha1989 <34509508+yancha1989@users.noreply.github.com>
2020-09-29 00:50:25 +0300

    Update 12.4.js
    еще раз исправил 4 задание после VSC

--- 12.4.js
+++ 12.4.js
@@ -1,58 +1,56 @@
-function ElecDevice(name, firm) {
-  (this.name = name), (this.firm = firm), (this.type = "electric");
+function ElecDevice(name, firm){
+  this.name = name,
+  this.firm = firm,  
+  this.type = 'electric'
 }
 
 ElecDevice.prototype.getPower = function eLectr(name, turnOn, passsport_power) {
-  let result = "";
-  if (turnOn === "turnOn") {
-    result =
-      "Потребляемая мощность прибора " +
-      name +
-      " " +
-      passsport_power * (1 / 3) +
-      " Ватт";
-  } else {
-    result = "Прибор выключен";
+  let result = '';
+  if(turnOn === "turnOn"){
+result = 'Потребляемая мощность прибора ' + name + ' ' + passsport_power * (1 / 3) + ' Ватт'}
+  
+  else {
+    result = "Прибор выключен"
   }
-  console.log(result);
-};
-
-function KitchenDevice(name, purpose) {
-  (this.name = name), (this.purpose = purpose);
+  console.log(result)
 }
 
-KitchenDevice.prototype = new ElecDevice();
-
-function BeautyDevice(name, firm, purpose) {
-  (this.name = name), (this.firm = firm), (this.purpose = purpose);
+function KitchenDevice(name, firm, purpose){
+  this.name = name,
+   this.firm = firm,
+  this.purpose = purpose  
 }
 
-BeautyDevice.prototype = new ElecDevice();
+KitchenDevice.prototype = new ElecDevice()
 
-KitchenDevice.prototype.getPower = function eLectr(
-  name,
-  turnOn,
-  passsport_power
-) {
-  let result = "";
-  if (turnOn === "turnOn") {
-    result =
-      "Если использовать " +
-      name +
-      " три раза за день, то потребляемая мощность составит " +
-      passsport_power * (1 / 3) +
-      " Ватт";
-  } else {
-    result = "Прибор выключен";
+function BeautyDevice(name, firm, purpose){
+  this.name = name,
+  this.firm = firm, 
+  this.purpose = purpose
+}
+
+BeautyDevice.prototype = new ElecDevice()
+
+
+KitchenDevice.prototype.getPower = function eLectr(name, turnOn, passsport_power) {
+  let result = '';
+  if(turnOn === "turnOn"){
+result = 'Если использовать ' + name + ' три раза за день, то потребляемая мощность составит ' + passsport_power * (1 / 3) + ' Ватт'}
+  
+  else {
+    result = "Прибор выключен"
   }
-  console.log(result);
-};
+  console.log(result)
+}
 
-const kettle = new KitchenDevice("Чайник", "Philips", "Подогрев воды");
-const hairDryer = new BeautyDevice("Фен", "Bosch", "Укладка волос");
 
-kettle.getPower("Чайник", "turnOn", 2100);
-hairDryer.getPower("turnOn", 900);
+const kettle = new KitchenDevice('Чайник','Philips', 'Подогрев воды')
+const hairDryer = new BeautyDevice ('Фен','Bosch', 'Укладка волос')
 
-console.log(hairDryer);
-console.log(kettle);
+
+kettle.getPower('Чайник', 'turnOn', 2100)
+hairDryer.getPower('turnOn', 900)
+
+console.log(hairDryer)
+console.log(kettle)
+
